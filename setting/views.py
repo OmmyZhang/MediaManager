673ceb8fd13e3aba42884ff304410805a682c725
张耀楠 <zhang-yn15@mails.tsinghua.edu.cn>
2017-10-13 23:44:10 +0800

    add some api and test for Belong

--- setting/views.py
+++ setting/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Belong
 
 # Create your views here.
+
+def createBelong(ui,gn):
+    b = Belong(user_id = ui , group_name = gn)
+    b.save()
+
+def checkBelong(ui,gn):
+    if Belong.objects.filter(user_id = ui , group_name = gn):
+        return True
+    else:
+        return False
+
+def groupMems(gn):
+    ui = []
+    for bb in Belong.objects.filter(group_name = gn):
+        ui.append(bb.user_id)
+    return ui
+
+def userGroups(ui):
+    gn = []
+    for bb in Belong.objects.filter(user_id = ui):
+        gn.append(bb.group_name)
+    return gn
+
+
+
+
