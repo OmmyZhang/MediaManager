c7c5c0f3593d06538120c611a6da67336a8a5fc6
张耀楠 <zhang-yn15@mails.tsinghua.edu.cn>
2017-10-21 17:48:40 +0800

    fix more bugs and delete useless url

--- tucloud/urls.py
+++ tucloud/urls.py
@@ -31,6 +31,5 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^accounts/',include('accounts.urls')),
-    url(r'^files/',include('files.urls')),
     url(r'^admin/', admin.site.urls),
 ]
