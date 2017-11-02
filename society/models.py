2d628343ffc08bf7bcc9bc3e0b239d295b91e747
ArtanisWei <wei-zy15@mails.tsinghua.edu.cn>
2017-11-02 09:24:52 -0700

    Finish the database logic write some test need to discuss about the interface

--- society/models.py
+++ society/models.py
@@ -6,15 +6,21 @@ from django.db import models
 class PeopleFollowPeople(models.Model):
 	followee = models.IntegerField();
 	follower = models.IntegerField();
+	def __str__(self):
+		return str(self.followee) + ' followed ' + str(self.follower)
 	
 class PeopleStarFile(models.Model):
 	people = models.IntegerField();
 	fileid = models.IntegerField();
+	def __str__(self):
+		return str(self.people) + ' stared ' + str(self.fileid)
 
 class PeopleComment(models.Model):
 	people = models.IntegerField();
 	fileid = models.IntegerField();
 	comment = models.CharField(max_length = 200);
+	def __str__(self):
+		return str(self.people) + '-->' + str(self.fileid) + '-->' self.comment
 	# This may not halal, I may try to deal with Chinese Character
 
 # Create your models here.
--- society/views.py
+++ society/views.py
@@ -2,32 +2,67 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-
+from .models import PeopleFollowPeople, PeopleStarFile, PeopleComment
 
 def followSb(idone, idtwo):   #idone follow idtwo or just cancel
-    pass
+    #followee follow the follower, this is not halal
+    if PeopleFollowPeople.object.filter(followee = idone, follower = idtwo):
+        PeopleFollowPeople.object.filter(followee = idone, follower = idtwo).delete()
+        return False
+    else:
+        PeopleFollowPeople.object.create(followee = idone, follower = idtwo)
+        # JUMPING : WE CAN SEND A MESSAGE HERE
+    return True
 
-def getFollowList(id): #get all the id followed
-    pass
+def getFollowerList(id): #get all the people this id followed
+    f = []
+    for id in PeopleFollowPeople.object.filter(followee = id):
+        f.append(id.follower)
+    return f
 
-def getFollowerList(id): # get all the follower
-    pass
+def getFolloweeList(id): # get all the follower
+    f = []
+    for id in PeopleFollowPeople.object.filter(follower = id):
+        f.append(id.followee)
+    return f
 
-def starFile(id, file): #idone star fileone
-    pass
+def starFile(id, file): #idone star fileone or just cancel
+    if PeopleStarFile.object.filter(people = id, fileid = file):
+        PeopleStarFile.object.filter(people=id, fileid=file).delete()
+        return False
+    else:
+        PeopleStarFile.object.create(people=id, fileid=file)
+        # JUMPING : WE CAN SEND A MESSAGE HERE
+    return True
 
 def getStarList(id): #get all the file stared
-    pass
+    f = []
+    for file in PeopleStarFile.object.filter(people = id):
+        f.append(file)
+    return f
 
 def getAllStarer(file): #get all the id stared the file
-    pass
+    f = []
+    for id in PeopleStarFile.object.filter(fileid = file):
+        f.append(id)
+    return
 
 def postComment(id, file, comment): #id post a comment about file
-    pass
+    if (len(comment) > 200):
+        return False
+    #Send ERROR HERE
+    PeopleComment.object.create(people = id, fileid = file, comment = comment)
+    # JUMPING : WE CAN SEND A MESSAGE HERE
+    return True
 
 def getComment(file): #get all comment about that tile
-    pass
+    f = []
+    for comment in PeopleComment.object.filter(fileid = file):
+        infotuple = (comment.id, comment.comment)
+        f.append(infotuple)
+    return f
 
 def removeFile(file): #remove the star and comment about a file
-    pass
+    PeopleStarFile.object.filter(fileid = file).delete()
+    PeopleComment.object.filter(fileid = file).delete()
+    return True
