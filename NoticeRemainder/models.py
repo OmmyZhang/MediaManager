68e2de107b41579cc80f4730f1bc19008a1127ae
Programming Bear <theweijy@gmail.com>
2017-11-03 15:20:48 +0800

    Add new feature to parse time

--- NoticeRemainder/models.py
+++ NoticeRemainder/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+class Notice(models.Model):
+    userId = models.IntegerField()
+    content = models.CharField(max_length = 200)
+    date = models.CharField(max_length = 100)
--- NoticeRemainder/views.py
+++ NoticeRemainder/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, logic, logout
+from django.contrib.auth.models import User
+import os,time
+import re
+from rest_framework import status, permissions
+from rest_framework.views import APIview
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAdminUser
+from setting.views import user_groups, group_mems, create_Belong
+from files.views import get_tag
 
-# Create your views here.
+class Remainder(APIView):
+    def get(self, request, format=None):
+        get = request.GET
+        user_id = get['userID']
+        after_time = get['afterTime']
+        notice_list = get_list(user_id, after_time)
+        return Response([formate_notice(i) for i in users])
+#----------------------------------
+
