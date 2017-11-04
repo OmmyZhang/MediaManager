2b34490e784200eee451753d4c9aa0c25398b4da
王润基 <wangrunji0408@163.com>
2017-11-04 21:25:45 +0800

    Fix file url.

--- files/urls.py
+++ files/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.FileList.as_view()),
-        url(r'^(?P<id>\d+)/$', views.FileById.as_view())
+        url(r'^(?P<id>\d+)/?$', views.FileById.as_view())
         ]
         
