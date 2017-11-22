210cca5365c811bd215908f463c6cc4ee21f404a
ArtanisWei <wei-zy15@mails.tsinghua.edu.cn>
2017-11-21 20:02:45 -0800

    Add more, still a long way to go

---
+++ accounts/TestSample.py
@@ -0,0 +1,6 @@
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import force_authenticate
+
+factory = APIRequestFactory()
+
+
--- society/TestSample.py
+++ society/TestSample.py
@@ -8,9 +8,10 @@ FollowPath2 = '/user/1/followint/3'
 FollowingPath = '/user/1/following'
 FollowerPath = '/user/2/follower
 CommentPath = '/comment'
+CommentPath_Get = '/comment?fileID=TESTFILE&type=star'
+#For get-method, we need a special check
 NullJson = {}
 
-CommentJson_Get = {}
 CommentJson_Star = {}
 CommentJson_Score = {}
 CommentJson_Comment = {}
@@ -37,9 +38,6 @@ CommentJson_Comment['date'] = '2017-11-11T02:39:42.490Z'
 CommentJson_Comment['type'] = 'comment'
 CommentJson_Comment['comment'] = 'This is a test'
 
-CommentJson_Get['fileID'] = 'TESTFILE'
-CommentJson_Get['type'] = 'star'
-
 #TEST : 1 FOLLOW 2
 request = factory.post(FollowPath1, NullJson, format='json')
 force_authenticate(request, user=user)  #This is not necessary
@@ -54,19 +52,19 @@ view = followSb.as_view()
 response = view(request)
 
 #TEST : GET FOLLOWING OF 1
-request = factory.GET(FollowingingPath, NullJson, format='json')
+request = factory.get(FollowingPath)
 force_authenticate(request, user=user)  #This is not necessary
 view = getFolloweeList.as_view()
 response = view(request)
 
 #TEST : GET FOLLOWER OF 2
-request = factory.GET(FollowerPath, NullJson, format='json')
+request = factory.get(FollowerPath)
 force_authenticate(request, user=user)  #This is not necessary
 view = getFolloweeList.as_view()
 response = view(request)
 
 #TEST : 1 UNFOLLOW 3
-request = factory.delete(FollowPath2, NullJson, format='json')
+request = factory.delete(FollowPath2)
 force_authenticate(request, user=user)  #This is not necessary
 view = followSb.as_view()
 response = view(request)
@@ -90,7 +88,7 @@ view = followSb.as_view()
 response = view(request)
 
 #TEST : GET ALL COMMENT
-request = factory.get(CommentPath, CommentJson_Get, format='json')
+request = factory.get(CommentPath_Get)
 force_authenticate(request, user=user)  #This is not necessary
 view = followSb.as_view()
 response = view(request)
