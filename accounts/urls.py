367d49b4cfa0c9c902d4d6f4b3116f63bbd71cfd
ArtanisWei <wei-zy15@mails.tsinghua.edu.cn>
2017-11-24 04:51:40 -0800

    Some little changes in urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
         url(r'^login/?$',  views.Login.as_view()),
         url(r'^logout/?$',  views.Logout.as_view()),
         url(r'^(?P<id>\d+)/?$', views.UserById.as_view()),
-        url(r'^(?P<id>\d+)/password/?$', views.Passwd.as_view())
+        url(r'^(?P<id>\d+)/password/?$', views.Passwd.as_view()),
+
+        url(r'^(?P<id1>\d+)/follower/?$', views.getFollowerList.as_view()),
+        url(r'^(?P<id1>\d+)/following/?$', views.getFolloweeList.as_view()),
+	url(r'^(?P<id1>\d+)/following/(?P<id2>\d+)/?$', views.followSb.as_view())
         ]
         
